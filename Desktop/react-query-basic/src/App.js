b5245ea523f09b55a102997ad5c78c01410c5243
seohyon-cho <flanique@naver.com>
2023-12-26 10:29:58 +0900

    react-query 이용 시 코드 효율성 증가 & 기본 개념 및 데이터 값 정의

--- Desktop/react-query-basic/src/App.js
+++ Desktop/react-query-basic/src/App.js
@@ -1,5 +1,6 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import UserInfo from './UserInfo';
 
 function App() {
 	// 전역 데이터 관리가 아니라, 비동기 데이터의 query key를 관리해주는 용도?..
@@ -9,6 +10,7 @@ function App() {
 		<QueryClientProvider client={queryClient}>
 			<div className='App'>
 				<h1>Hello</h1>
+				<UserInfo />
 			</div>
 			<ReactQueryDevtools />
 		</QueryClientProvider>
