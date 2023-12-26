1a18f7f213eebf9b2a9a070a8a517527c7cda5c0
seohyon-cho <flanique@naver.com>
2023-12-26 09:59:27 +0900

    react-query 및 전용 개발자 도구 셋팅

--- Desktop/react-query-basic/src/App.js
+++ Desktop/react-query-basic/src/App.js
@@ -1,5 +1,18 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 function App() {
-	return <div className='App'></div>;
+	// 전역 데이터 관리가 아니라, 비동기 데이터의 query key를 관리해주는 용도?..
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className='App'>
+				<h1>Hello</h1>
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
